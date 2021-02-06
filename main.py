2e427261eb14ff186f14118d34dfa53b909b3a63
Manuel Santiago <msanti16@jhu.edu>
2021-02-06 14:27:34 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+from mod01_modularity.words import main_words
+
+
+print('main.py __name__ : ')
+print(__name__)
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    print(url)
+    main_words(url)
